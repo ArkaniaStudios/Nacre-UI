a991118fb0933789c184122661e21ac4c769c5c4
Dumont-Julien <dumontj357@gmail.com>
2024-02-21 13:43:41 +0100

    Add: Class icon

---
+++ src/form/elements/icon/IconPath.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace arkania\form\elements\icon;
+
+final class IconPath implements Icon {
+
+    private string $path;
+
+    public function __construct(
+        string $path
+    ) {
+        $this->path = $path;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function jsonSerialize() : array {
+        return [
+            "type" => "path",
+            "data" => $this->path
+        ];
+    }
+
+}
---
+++ src/form/elements/icon/IconUrl.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace arkania\form\elements\icon;
+
+final class IconUrl implements Icon {
+
+    private string $url;
+
+    public function __construct(
+        string $url
+    ) {
+        $this->url = $url;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function jsonSerialize() : array {
+        return [
+            "type" => "url",
+            "data" => $this->url
+        ];
+    }
+
+}
