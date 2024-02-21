ac6e4187215c6b9f68d430d036b25a54ad8cbb34
Dumont-Julien <dumontj357@gmail.com>
2024-02-21 14:06:11 +0100

    Add: Implementation des boutons.

--- src/form/elements/buttons/Button.php
+++ src/form/elements/buttons/Button.php
@@ -3,10 +3,47 @@ declare(strict_types=1);
 
 namespace arkania\form\elements\buttons;
 
+use arkania\form\elements\icon\Icon;
+use arkania\form\trait\FormTranslationTrait;
+use arkania\form\trait\PermissibleTrait;
 use JsonSerializable;
+use pocketmine\lang\Translatable;
+use pocketmine\permission\Permission;
 
 abstract class Button implements JsonSerializable {
+    use FormTranslationTrait;
+    use PermissibleTrait;
 
-    public function __construct() {}
+    private string $name;
+    private Translatable|string $text;
+    private Icon|null $icon;
+    private Permission|string|null $permission = null;
+
+    public function __construct(
+        string $name,
+        Translatable|string $text,
+        Permission|string|null $permission = null,
+        Icon|null $icon = null
+    ) {
+        $this->name = $name;
+        $this->text = $text;
+        $this->icon = $icon;
+        if($permission !== null) {
+            $this->setPermission($permission);
+        }
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+    public function jsonSerialize() : array {
+        return [
+            "text" => $this->translate($this->text),
+            "name" => $this->name,
+            "icon" => $this->icon?->jsonSerialize() ?? null,
+            "permission" => $this->permission instanceof Permission ? $this->permission->getName() : $this->permission
+        ];
+    }
 
 }
---
+++ src/form/elements/buttons/SimpleButton.php
@@ -0,0 +1,6 @@
+<?php
+declare(strict_types=1);
+
+namespace arkania\form\elements\buttons;
+
+final class SimpleButton extends Button {}
