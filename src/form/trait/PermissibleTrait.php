bc9ee7f7d03b2d7703559d30b35c9d342a599936
Dumont-Julien <dumontj357@gmail.com>
2024-02-21 19:42:02 +0100

    Update: Modification des traits

--- src/form/trait/PermissibleTrait.php
+++ src/form/trait/PermissibleTrait.php
@@ -4,21 +4,27 @@ declare(strict_types=1);
 namespace arkania\form\trait;
 
 use pocketmine\permission\Permission;
+use pocketmine\player\Player;
 
 trait PermissibleTrait {
+    public function __construct(Player $player) {
+        $this->player = $player;
+    }
 
-     private Permission|string|null $permission = null;
+    protected Permission|string|null $permission = null;
 
-        public function setPermission(Permission|string $permission) : void {
-            $this->permission = $permission;
-        }
+    public function setPermission(Permission|string $permission) : void {
+        $this->permission = $permission;
+    }
 
-        public function getPermission() : Permission|string|null {
-            return $this->permission;
-        }
+    public function getPermission() : Permission|string|null {
+        return $this->permission;
+    }
 
-        public function hasPermission() : bool {
-            return $this->player->hasPermission($this->permission);
-        }
+    public function hasPermission() : bool {
+        if($this->permission === null)
+            return true;
+        return $this->player->hasPermission($this->permission);
+    }
 
 }
