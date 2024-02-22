f4e0a371ceea9e684d78685415de56852b531280
Dumont-Julien <dumontj357@gmail.com>
2024-02-22 23:32:05 +0100

    Add: Ajout des cutoms form

---
+++ src/form/elements/customs/StepSlider.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace arkania\form\elements\customs;
+
+use arkania\form\elements\Element;
+
+final class StepSlider extends Element {
+
+    private string $text;
+    private int $default;
+
+    /** @var mixed[] */
+    private array $step;
+
+
+    public function __construct(
+        string $name,
+        string $text = '',
+        array $step = [],
+        int $default = 0
+    ) {
+        parent::__construct($name);
+        $this->text = $text;
+        $this->default = $default;
+        $this->step = $step;
+    }
+
+    public function getType() : string {
+        return 'step_slider';
+    }
+
+    public function handler($data) : mixed {
+        return $this->step[$data];
+    }
+
+    /**
+     * @return array<string, string|mixed[]>
+     */
+    public function jsonSerialize() : array {
+        return [
+            'type' => $this->getType(),
+            'text' => $this->text,
+            'steps' => $this->step,
+            'default' => $this->default
+        ];
+    }
+
+}
---
+++ src/form/elements/customs/Toggle.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace arkania\form\elements\customs;
+
+use arkania\form\elements\Element;
+
+final class Toggle extends Element {
+
+    private string $text;
+    private bool $default;
+
+    public function __construct(
+        string $name,
+        string $text = '',
+        bool $default = false
+    ) {
+        parent::__construct($name);
+        $this->text = $text;
+        $this->default = $default;
+    }
+
+    public function getType() : string {
+        return 'toggle';
+    }
+
+    public function handler($data) : bool {
+        return (bool)$data;
+    }
+
+    /**
+     * @return array<string, string|bool>
+     */
+    public function jsonSerialize() : array {
+        return [
+            'type' => $this->getType(),
+            'text' => $this->text,
+            'default' => $this->default
+        ];
+    }
+
+}
